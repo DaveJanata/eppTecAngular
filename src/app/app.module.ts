06deb65c90e6b6dc909db80925a61c15abb5459d
DaveJ <janatadavid.95@gmail.com>
2022-10-01 17:56:47 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './components/post/post.component';
+import { CommentButtonComponent } from './components/comment-button/comment-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    CommentButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/comment-button/comment-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-button',
+  templateUrl: './comment-button.component.html',
+  styleUrls: ['./comment-button.component.css']
+})
+export class CommentButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
